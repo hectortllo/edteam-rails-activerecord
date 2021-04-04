4f2dc9e52f298f6a537911bac2e5608808cf2838
Héctor Tello <hectortllo@gmail.com>
2021-04-03 22:07:48 -0600

    Video callbacks

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,22 @@
 class Profile < ApplicationRecord
+  after_initialize do |profile|
+    puts "You have initialized an object"
+  end
+
+  after_find do |profile|
+    puts "You have found an object!"
+  end
+
+  after_create :show_message
+
   validates :full_name, presence: true
   validates :email, presence: true
   validates :phone_number, length: { is: 8 }
   validates :age, numericality: { only_integer: true }
   validates :email, uniqueness: true
+
+  private
+  def show_message
+    puts "You have created an object!"
+  end
 end
