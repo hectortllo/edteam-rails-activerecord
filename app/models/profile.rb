635fb913a2a0746a4f0ef89c462342ea78037969
Héctor Tello <hectortllo@gmail.com>
2021-04-03 21:05:17 -0600

    Video validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,7 @@
 class Profile < ApplicationRecord
+  validates :full_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, length: { is: 8 }
+  validates :age, numericality: { only_integer: true }
+  validates :email, uniqueness: true
 end
